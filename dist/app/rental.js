e34b53ce1f0fb84b47747740b6106a58e8644079
shikai93 <nsk93@hotmail.com>
2016-09-29 06:02:00 +0800

    connect dataBase

--- dist/app/rental.js
+++ dist/app/rental.js
@@ -44,10 +44,11 @@ angular
   .module('rental')
   .controller('SearchController', function($scope, supersonic) {
     $scope.navbarTitle = "Search";
+    
     $scope.getDetails = function() {
     
         supersonic.data.model('Product').findAll({query: JSON.stringify({"Pid": 1})}).then(function(Products) {
-
+            
             supersonic.data.model('Users').findAll({query: JSON.stringify({"Uid": Products[0].Uid})}).then(function(Users) {
 
             localStorage.setItem('user', Users[0].Username);
