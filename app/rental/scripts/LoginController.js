d8414de62232ec38f0dfc4e60d486b05431ebd38
shikai93 <nsk93@hotmail.com>
2016-10-26 06:17:07 +0800

    got to log in and requesting for permission page

--- app/rental/scripts/LoginController.js
+++ app/rental/scripts/LoginController.js
@@ -1,7 +1,16 @@
 angular
-  .module('rental')
-  .controller('LoginController', function($scope, supersonic, $http) {
+  .module('rental',  ["ionic", "ngCordova"])
+  .controller('LoginController', ["$scope", "$cordovaOauth", "$http", function($scope, supersonic, $cordovaOauth, $http) {
+    
+    window.cordovaOauth = $cordovaOauth;
+    window.http = $http;
+    
+    $scope.changeView = function () {
 
+       var view = new supersonic.ui.View("rental#newListing");
+        supersonic.ui.layers.push(view);
+    };
+    
     $scope.navbarTitle = "Login";
     $scope.logme = "I'm waiting!!";
     //steroids.addon.facebook.ready.then(funtion(){
@@ -10,4 +19,4 @@ angular
       // });
 
 
-  });
+  }]);
