e9f9d3479cc81500b6c20a67df8e1ae420d6bb99
Natalie Chin <chin_natalie@yahoo.com.sg>
2016-10-26 20:46:00 -0500

    added my listings on myAccount page

--- app/rental/scripts/myAccountController.js
+++ app/rental/scripts/myAccountController.js
@@ -4,4 +4,13 @@ angular
       $scope.loadProfile = function () {
           $scope.account = { 'name': 'Brian Walker', 'location': 'Evanston, IL', 'email': 'brian@nu.com', 'number': '(123) 567-789', 'description': 'I have many things to rent out' };
       };
+
+      $scope.myThings = [
+          {
+              'Name': 'Skateboard',
+              'Img': 'http://www.bonkersenergy.com/wp-content/uploads/2015/07/skateboard_01.jpg',
+              'Rate': 5,
+              'Rating': 5
+          }
+      ];
   });
