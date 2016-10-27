91078f8ba12362a19c3ff802d332315f24fdeeb5
shikai93 <nsk93@hotmail.com>
2016-10-27 11:33:41 +0800

    merge properly

--- app/rental/scripts/LoginController.js
+++ app/rental/scripts/LoginController.js
@@ -1,15 +1,11 @@
 angular
   .module('rental')
   .controller('LoginController', function($scope, supersonic, $http) {
-<<<<<<< HEAD
-    $scope.fbusername= "";
-=======
     $scope.hideMe = true;
     if(localStorage.getItem('user') === null){
       $scope.hideMe = false;
     }
-    $scope.fbusername= "Use Facebook Username";
->>>>>>> 977c55d203d5ccafc2d4e97a7a547555eb1bfc48
+    $scope.fbusername= "";
     $scope.logme = function(){      //if the box isnt empty
       if($scope.fbusername.length > 0){
         //if no userid is found in local storage
@@ -32,7 +28,7 @@ angular
                             url : "http://naybro-node.mybluemix.net/signup",
                             params : {
                               username : $scope.fbusername
-                            }})
+                            }});
                             $scope.logme();
 
                         }
